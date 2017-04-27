d45dc517aa399bc3fb4add8c850ca63516cea23b
Radost Stanimirova <rkstan@geo.bu.edu>
2017-04-27 14:44:11 -0400

    edited scripts to calculate standardized anomalies and added a script to filter the MOD13C2 EVI timeseries based on QA/QC information

---
+++ scripts/01_calculate_temp_anomalies.R
@@ -0,0 +1,152 @@
+# This is a script to read in temperature data from CRU at 0.5-degree
+# and calculate monthly seasonal standardized anomalies in mean temperature
+# created by Radost Stanimirova April 2017
+
+# init ----------------------
+
+# load libraries ------------------
+library(ncdf4)
+library(chron)
+library(RColorBrewer)
+library(lattice)
+
+library(sp)
+library(raster)
+library(rgdal)
+library(gtools)
+library(maptools)
+
+# set functions ----------------------
+
+# make a matrix of all the z score values from a raster stack
+get_raster_vals <- function(data){
+  
+  z_values <- matrix(NA, nrow=259200,ncol=dim(data)[3])
+  for (i in 1:dim(data)[3]) {
+    sub_values1 <- getValues(data[[i]])
+    
+    if(i==1){
+      z_values <-sub_values1
+      
+    }else{
+      z_values <- cbind(z_values, sub_values1)
+      
+    }
+  }
+  return(z_values)
+  
+}
+
+# assign in variables ------------------------
+year <- rep(1:13, each=12)
+m_year <- rep(1:12, 13)
+seasons <- rep(c(1,1,2,2,2,3,3,3,4,4,4,1), 13)
+index <- rep(1:13, each=3)
+
+# read in South America boundary 
+data(wrld_simpl)
+SA <- c("Argentina", "Bolivia", "Brazil", "Chile", "Colombia", "Ecuador", "Guyana", 
+        "Paraguay", "Peru", "Suriname", "Uruguay", "Venezuela")
+sa <- wrld_simpl[which(wrld_simpl@data$NAME %in% SA),]
+
+
+# read in pastureland extent data --------------------------
+pasture_2000_sub <- raster("/projectnb/modislc/users/rkstan/GE712/data/pasture_extent/pasture_2000_sub.tif")
+pasture_2000_sub[pasture_2000_sub<0.6] <- NA
+
+
+# read in CRU data --------------------------
+temp <- brick("/projectnb/modislc/users/rkstan/GE712/data/cru.ts4.00/cru_ts4_SA.tif") # read in the red SDS as a raster
+temp_time_sub <- temp[[1225:1380]] # subset only desired time period 2003-2016
+temp_sub_unstack <- unstack(temp_time_sub) # unstack the raster brick into raster layers
+
+temp <- values(temp_time_sub)
+
+
+# calculate monthly temperature anomalies --------------------------------
+
+# calculate the anomalies for each month (e.g., subtract January temp from mean January temperature for 14 year time span)
+for (t in 1:13){
+  for (m in 1:12){
+    # calculate the mean temperature of each month over the whole 14 year period (excluding each time the month of interest)
+    temp_month_mean <- calc(dropLayer(stack(temp_sub_unstack[m_year==m]), t),mean, na.rm=T)
+    temp_month_sd <- calc(dropLayer(stack(temp_sub_unstack[m_year==m]), t),sd, na.rm=T) # calculate the standard deviation 
+    
+    # calculate the anomaly 
+    temp_anomaly <- (stack(temp_sub_unstack[m_year==m]) - temp_month_mean)/temp_month_sd
+    
+    if(m==1){
+      temp_monthly_anomalies <- temp_anomaly
+      
+    }else{
+      temp_monthly_anomalies  <- append(temp_monthly_anomalies, temp_anomaly)    
+      
+    }
+    
+  }
+  
+}
+
+
+# get the anomalies in a matrix and save as csv 
+temp_monthly_anomalies_vals <- get_raster_vals(stack(temp_monthly_anomalies))
+write.csv(temp_monthly_anomalies_vals, file="/projectnb/modislc/users/rkstan/GE712/outputs/temp_monthly_anomalies.csv", quote=FALSE, row.names=FALSE)
+
+# calculate seasonal temperature anomalies --------------------------------
+for (t in 1:13){
+  for (m in 1:4){
+    # calculate the mean temperature of each month over the whole 14 year period (excluding each time the month of interest)
+    temp_seas_mean <-calc(dropLayer(stack(temp_sub_unstack[seasons==m]), which(index==t, arr.ind=TRUE)),mean, na.rm=T)
+    temp_seas_sd <- calc(dropLayer(stack(temp_sub_unstack[seasons==m]), which(index==t, arr.ind=TRUE)),sd, na.rm=T) # calculate the standard deviation 
+    temp_mean <- stackApply(stack(temp_sub_unstack[seasons==m]), index, mean, na.rm=T)
+    
+    # calculate the anomaly 
+    temp_anomaly <- (temp_mean - temp_seas_mean)/temp_seas_sd
+    
+    if(m==1){
+      temp_seas_anomalies <- temp_anomaly
+      
+    }else{
+      temp_seas_anomalies  <- append(temp_seas_anomalies, temp_anomaly)    
+      
+    }
+    
+  }
+  
+}
+
+# get the seasonal anomalies in a matrix and save as csv 
+temp_seas_anomalies_vals <- get_raster_vals(stack(temp_seas_anomalies))
+write.csv(temp_seas_anomalies_vals, file="/projectnb/modislc/users/rkstan/GE712/outputs/temp_seas_anomalies.csv", quote=FALSE, row.names=FALSE)
+
+# plot monthly anomalies -------------------------------------------------
+names(temp_monthly_anomalies[[3]]) <- paste0("Y", 2003:2015)
+spplot(temp_monthly_anomalies[[3]], bty='n',xaxt='n', yaxt='n', box=FALSE)
+spSA = list("sp.polygons", sa)
+spplot(temp_monthly_anomalies[[3]], bty='n', xaxt='n', yaxt='n', box=FALSE, sp.layout=spSA)
+
+
+plot(temp_monthly_anomalies[[3]][[5]],col=brewer.pal(6, "RdBu"), xlim=c(xmin(temp_monthly_anomalies[[3]][[5]]), xmax(temp_monthly_anomalies[[3]][[5]])),ylim=c(ymin(temp_monthly_anomalies[[3]][[5]]), ymax(temp_monthly_anomalies[[3]][[5]])),
+     cex.axis=1, cex.lab=1.5, legend=FALSE, axes=F, box=F) 
+plot(sa, add=T,lty=1,lwd=0.5)
+#axis(side=1,pos=ymin(precip_anomaly[[8]]),at=seq(xmin(precip_anomaly[[8]]),xmax(precip_anomaly[[8]]),10), cex.axis=1.25)
+#axis(side=2,pos=xmin(precip_anomaly[[8]]),at=seq(ymin(precip_anomaly[[8]]),ymax(precip_anomaly[[8]]),10), cex.axis=1.25)
+#title(xlab="Longitude", cex.lab=1.25, line=2.5)
+#title(ylab="Latitude", cex.lab=1.25, line=2.5)
+#color.bar(igbpcol_real,1,17)
+#legend("right", inset=c(-0.4,-0.4), legend="Land Cover Class")
+plot(temp_monthly_anomalies[[3]][[5]], col=brewer.pal(6, "RdBu"),legend.only=TRUE, legend.width=1, legend.shrink=1,legend.args=list("Temperature Standardized Anomalies", side=4, line=2.9, cex=1.25))
+
+# plot seasonal anomalies -------------------------------------------------
+names(temp_seas_anomalies[[3]]) <- paste0("Y", 2003:2015)
+spplot(temp_seas_anomalies[[3]], bty='n',xaxt='n', yaxt='n', box=FALSE)
+spSA = list("sp.polygons", sa)
+spplot(temp_seas_anomalies[[3]], bty='n', xaxt='n', yaxt='n', box=FALSE, sp.layout=spSA)
+
+
+plot(temp_seas_anomalies[[3]][[5]],col=brewer.pal(6, "RdBu"), xlim=c(xmin(temp_seas_anomalies[[3]][[5]]), xmax(temp_seas_anomalies[[3]][[5]])),ylim=c(ymin(temp_seas_anomalies[[3]][[5]]), ymax(temp_seas_anomalies[[3]][[5]])),
+     cex.axis=1, cex.lab=1.5, legend=FALSE, axes=F, box=F) 
+plot(sa, add=T,lty=1,lwd=0.5)
+plot(temp_seas_anomalies[[3]][[5]], col=brewer.pal(6, "RdBu"),legend.only=TRUE, legend.width=1, legend.shrink=1,legend.args=list("Temperature Standardized Anomalies", side=4, line=2.9, cex=1.25))
+
+
---
+++ scripts/03_calculate_EVI_QC.R
@@ -0,0 +1,131 @@
+# read in required libraries ------------------------
+library(sp)
+library(rgdal)
+#library(tiff)
+library(raster)
+library(lubridate)
+library(maptools)
+
+# set up to run in parallel 
+library(parallel)
+cl <- makeCluster(8)
+clusterEvalQ(cl, {library(raster)})
+
+# define functions --------------------------
+# make a matrix of all the z score values from a raster stack
+get_raster_vals <- function(data){
+  
+  z_values <- matrix(NA, nrow=259200,ncol=dim(data)[3])
+  for (i in 1:dim(data)[3]) {
+    sub_values1 <- getValues(data[[i]])
+    
+    if(i==1){
+      z_values <-sub_values1
+      
+    }else{
+      z_values <- cbind(z_values, sub_values1)
+      
+    }
+  }
+  return(z_values)
+  
+}
+
+
+# assign in variables ------------------------
+start.year <- "2003"
+end.year <- "2015"
+
+data(wrld_simpl)
+SA <- c("Argentina", "Bolivia", "Brazil", "Chile", "Colombia", "Ecuador", "Guyana", 
+        "Paraguay", "Peru", "Suriname", "Uruguay", "Venezuela")
+sa <- wrld_simpl[which(wrld_simpl@data$NAME %in% SA),]
+
+# read in data ----------------------------
+
+pasture_2000_sub <- raster("/projectnb/modislc/users/rkstan/GE712/data/pasture_extent/pasture_2000_sub.tif")
+pasture_2000_sub[pasture_2000_sub<0.6] <- NA
+
+path_MOD13C2 <- "/projectnb/modislc/users/rkstan/GE712/data/MOD13C2/" # path to your MOD13C2 files
+r <- raster(nrows=600, ncols=700)
+rowcol <- rowColFromCell(r, c(1:420000))
+
+GetEVI <- function(MOD13C2_file_path, as.int=T){
+  fun <- function(x) {x[x==32767] <- NA; return(x)}
+  
+  SDS_EVI <- MOD13C2_file_path
+  evi <- raster(SDS_EVI) # read in the red SDS as a raster
+  NAvalue(evi) <- 32767
+  evi <- calc(evi, fun)
+  return(evi)
+  
+}
+
+# set up parallel analysis 
+clusterExport(cl, c("GetEVI"))
+
+MOD13C2_tile_files <- Sys.glob(file.path(path_MOD13C2, paste("MOD13C2", "*", '006',"*",'SA.clip', "tif", sep=".")))
+# calculate EVI
+evi_list <- parLapply(cl, MOD13C2_tile_files, GetEVI)
+evi_stack <- stack(unlist(evi_list)) # unlist and convert to a single RasterStack
+
+evi_final <- evi_stack/10000
+evi_vals <- values(evi_final)
+
+evi_vals[which(evi_vals<0 & !is.na(evi_vals))] <- NA
+
+path_MOD13C2 <- "/projectnb/modislc/users/rkstan/GE712/data/MOD13C2/"
+MOD13C2_tile_files <- Sys.glob(file.path(path_MOD13C2, paste("MOD13C2", "*", '006',"*", 'SA.QA.clip', "tif", sep=".")))
+
+GetQC <- function(MOD13C2_file_path, as.int=T){
+  
+  SDS_QC <- MOD13C2_file_path
+  qc <- raster(SDS_QC) # read in the red SDS as a raster
+  return(qc)
+  
+}
+
+q_list <- parLapply(cl, MOD13C2_tile_files, GetQC)
+q_stack <- stack(unlist(q_list)) # unlist and convert to a single RasterStack
+qc <- values(q_stack)
+
+
+qcv <- as.vector(as.matrix(qc))
+
+qc_00_01 <- rep(NA, length(qc))
+qc_02_05 <- rep(NA, length(qc))
+qc_06_07 <- rep(NA, length(qc))
+qc_08 <- rep(NA, length(qc))
+qc_10 <- rep(NA, length(qc))
+qc_fin <- rep(NA, length(qc))
+#BinToDec <- function(x) sum(2^(which(unlist(strsplit(as.character(x), "")) ==1)-1))
+
+for (t in 1:length(qcv)){
+  
+  quality_bit <- as.numeric(intToBits(qcv[t]))
+  # bits 0 & 1
+  qc_00_01[t] <- 1.*quality_bit[1] + 2.*quality_bit[2]
+  # bits 2-5 VI usefulness
+  qc_02_05[t] <- 1.*quality_bit[3] + 2.*quality_bit[4]+4.*quality_bit[5]+8.*quality_bit[6]
+  # bits 6-7 aerosol
+  qc_06_07[t] <- 1.*quality_bit[7] + 2.*quality_bit[8]
+  # bit 8 Adjacent cloud detected
+  qc_08[t] <- 1.*quality_bit[9]
+  # bit 10 Mixed clouds
+  qc_10[t] <- 1.*quality_bit[11] 
+  
+}
+
+qc_fin[(qc_00_01==1 | qc_00_01==0) & (qc_02_05>=0 & qc_02_05<=11) & (qc_08==0) & (qc_10==0) & (qc_06_07>=1 & qc_06_07<=2)]=1
+#qc_index <- qc_fin==0
+#evi_vals[which(qc_fin==0)]<-NA
+evi_vals[is.na(qc_fin)]<-NA
+
+south_am <- brick(nrows=1380, ncols=940, nl=156, crs="+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0",
+                  xmn=-81.5, xmx=-34.5, ymn=-56.5, ymx=12.5)
+south_am <- setValues(south_am, evi_vals)
+
+setwd("/projectnb/modislc/users/rkstan/GE712/data/MOD13C2/")
+writeRaster(south_am, file="filtered_EVI.tif", format="GTiff", overwrite=TRUE)
+
+
