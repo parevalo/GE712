24c8b8acdff65739b6fcf910a52fc8afccf37377
Radost Stanimirova <rkstan@geo.bu.edu>
2017-08-15 07:40:48 -0400

    modified script to implement the apply_stack_parallel.R function

--- scripts/read_cci.R
+++ scripts/read_cci.R
@@ -1,9 +1,12 @@
+source("/projectnb/modislc/users/rkstan/GE712/GE712/scripts/apply_stack_parallel.R")
+
+# load libraries 
 library(RColorBrewer)
 library(raster)
 library(maptools)
 
 
-# function to remove 32767 fill value 
+# function to set all irrelevant classes to NA. Keep only the classes listed here
 fun <- function(x) {x[!x==30 & !x==40 & !x==110 & !x==130 & !x==140 & !x==150 
                       & !x==151 & !x==152 & !x==153] <- NA; return(x)}
 
@@ -13,26 +16,39 @@ SA <- c("Argentina", "Bolivia", "Brazil", "Chile", "Colombia", "Ecuador", "Guyan
         "Paraguay", "Peru", "Suriname", "Uruguay", "Venezuela")
 sa <- wrld_simpl[which(wrld_simpl@data$NAME %in% SA),]
 
-#read CCI data 
+# #read CCI data for South America and select only time period of interest (2003-2015)
+# #cci <- stack("/projectnb/modislc/users/rkstan/GE712/data/CCI/ESACCI-LC-L4-LCCS-Map-300m-P1Y-1992_2015-v2.0.7.tif")
+# cci_sa <- stack("/projectnb/modislc/users/rkstan/GE712/data/CCI/land_cover_change_sa.tif")
+# #cci_time <- cci[[12:24]]
+# cci_sa_time <- cci_sa[[12:24]] 
+# 
+# # Keep only the classes we need and save the raster
+# cci_pasture_sub <-  calc(cci_sa_time, fun)
+# setwd("/projectnb/modislc/users/rkstan/GE712/outputs")
+# writeRaster(cci_pasture_sub, file= "cci_pasture_sub.hdr", format="ENVI", overwrite=TRUE)
 
-cci <- stack("/projectnb/modislc/users/rkstan/GE712/data/CCI/ESACCI-LC-L4-LCCS-Map-300m-P1Y-1992_2015-v2.0.7.tif")
-cci_sa <- stack("/projectnb/modislc/users/rkstan/GE712/data/CCI/land_cover_change_sa.tif")
-#cci_clip <- stack("/projectnb/modislc/users/rkstan/GE712/data/CCI/land_cover_change_clip.tif")
-#cci_pasture <- stack("/projectnb/modislc/users/rkstan/GE712/data/CCI/land_cover_change_pasture.tif") 
-cci_time <- cci[[12:24]] 
-cci_sa_time <- cci_sa[[12:24]] 
+# read in raster subset (in order to avoid running the first part again as it is slow!)
+cci_pasture_sub <- brick("/projectnb/modislc/users/rkstan/GE712/outputs/cci_pasture_sub.envi")
 
-cci_pasture_sub <-  calc(cci_sa_time, fun)
-#b4 <- calc(cci_pasture_sub, fun=function(x,...){length(unique(x))})
-
-lcc_mask <- calc(cci_pasture_sub, fun=function(x,...){ if (is.na(x[1])){ NA } else {length(unique(x))}})
-#b1 <- calc(cci_pasture_sub, fun=function(x){sum(x==30) | sum(x==40) | sum(x==110) | sum(x==130)| sum(x==140)|
-#                                              sum(x==150) | sum(x==151) | sum(x==152) | sum(x==153)})
-
-setwd("/projectnb/modislc/users/rkstan/GE712/outputs")
-writeRaster(lcc_mask, file= "lcc_mask.hdr", format="ENVI", overwrite=TRUE)
-
+get_stat <- function(x, args.list){
   
+  res <- rep(NA, args.list$nl)
+  
+ # if (anyNA(x))
+#    return(res)
+  
+  n <- sum(is.na(x))
+  x[n>4]<-NA
+  x[is.na(x)] <- -9999
+  unique_vals = length(unique(x))
+  res <- c(uv = unique_vals)
+
+return(res)
+
+} 
+
+lcc_mask = apply_stack_parallel(cci_pasture_sub, fun =get_stat, nl = 1)
+
 # plot ------------------------------------------------------------------
 
 cbPallete <- c(brewer.pal(6, "Paired"))
@@ -54,3 +70,15 @@ par(fig=c(0,1,0,1), oma=c(0, 0, 0, 0), mar=c(0,0,0,0), pty="s", new=TRUE)
 plot(0,0, type="n", bty="n", xaxt="n", yaxt="n")
 legend("bottom", fill=cbPallete, legend = c('1', '2', '3'), horiz=T)
 
+# Extras ------------------------------------------------------------------
+#b4 <- calc(cci_pasture_sub, fun=function(x,...){length(unique(x))})
+#lcc_mask <- calc(cci_pasture_sub, fun=function(x,...){ if (is.na(x[1])){ NA } else {length(unique(x))}})
+#lcc_mask <- calc(cci_pasture_sub, fun=function(x,...) {length(unique(na.pass(x)))})
+
+#b1 <- calc(cci_pasture_sub, fun=function(x){sum(x==30) | sum(x==40) | sum(x==110) | sum(x==130)| sum(x==140)|
+#                                              sum(x==150) | sum(x==151) | sum(x==152) | sum(x==153)})
+
+# writeRaster(b4, file= "lcc_mask.hdr", format="ENVI", overwrite=TRUE)
+# n <- stackApply(cci_pasture_sub, indices=1, fun=function(x,...){sum(is.na(x))})
+# cci_pasture_sub[is.na(cci_pasture_sub)] <- -9999
+# 
